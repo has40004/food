eb54aeb8627dfd60407ee9f6189bbd2ebb689bc7
hasan <alsamman@mail.ru>
2020-12-16 15:42:59 +0300

    json-server

--- js/script.js
+++ js/script.js
@@ -298,6 +298,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     }
     
+    fetch('http://localhost:3000/menu')
+    .then(data => data.json())
+    .then(res => console.log(res));
 
 });
 
