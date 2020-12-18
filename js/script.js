de337032061835fac7b3077791fabc4e2d8e372a
hasan <alsamman@mail.ru>
2020-12-18 15:19:52 +0300

    sliders 2part

--- js/script.js
+++ js/script.js
@@ -289,47 +289,117 @@ window.addEventListener('DOMContentLoaded', () => {
           prev = document.querySelector('.offer__slider-prev'),
           next = document.querySelector('.offer__slider-next'),
           total = document.querySelector('#total'),
-          current = document.querySelector('#current');
+          current = document.querySelector('#current'),
+          slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+          slidesField = document.querySelector('.offer__slider-inner'),
+          width = window.getComputedStyle(slidesWrapper).width;
 
     let slideIndex = 1;
-    slideShow (slideIndex);
-
+    let offset = 0;
     if (slides.length < 10){
         total.textContent = ` 0${slides.length}`;
-       
+        current.textContent = ` 0${slideIndex}`;
     } else {
         total.textContent = slides.length;
+        current.textContent = slideIndex;
     }
 
-    function slideShow (n) {
+    slidesField.style.width = 100 * slides.length + '%';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '1.5s all';
 
-        if ( n > slides.length){
+    slidesWrapper.style.overflow = 'hidden';
+
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+    
+    next.addEventListener('click', () => {
+        if ( offset == +width.slice(0, width.length - 2) * (slides.length - 1)){
+            offset= 0;
+            
+        }else {
+            offset += +width.slice(0, width.length -2);
+            console.log(offset);
+        }
+
+        slidesField.style.transform = ` translateX(-${offset}px)`;
+
+        if ( slideIndex == slides.length){
             slideIndex = 1;
+        }else {
+            slideIndex++;
         }
 
-        if ( n < 1 ) {
-            slideIndex = slides.length;
-        }
-        slides.forEach(item => {
-            item.style.display = 'none';
-        });
-        slides[slideIndex - 1].style.display = 'block';
-
-        if (slides.length < 10){
-            current.textContent = ` 0${slideIndex }`;
+        if ( slides.length < 10){
+            current.textContent = ` 0${slideIndex}`;
         } else {
-            current.textContent = slideIndex ;
+            current.textContent = slideIndex;
         }
-    }
-
-    prev.addEventListener('click', () => {
-        slideShow(slideIndex -= 1);
-
     });
 
-    next.addEventListener('click', ()=> {
-        slideShow(slideIndex += 1);
+     prev.addEventListener('click', () => {
+        if ( offset == 0){
+            offset = +width.slice(0, width.length -2) * (slides.length - 1);
+        }else {
+            offset -= +width.slice(0, width.length -2);
+        }
+
+        slidesField.style.transform = ` translateX(-${offset}px)`;
+
+        if ( slideIndex == 1){
+            slideIndex = slides.length;
+        }else {
+            slideIndex--;
+        }
+
+        if ( slides.length < 10){
+            current.textContent = ` 0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
     });
+    
+    
+
+    // slideShow (slideIndex);
+
+    // if (slides.length < 10){
+    //     total.textContent = ` 0${slides.length}`;
+       
+    // } else {
+    //     total.textContent = slides.length;
+    // }
+
+    // function slideShow (n) {
+
+    //     if ( n > slides.length){
+    //         slideIndex = 1;
+    //     }
+
+    //     if ( n < 1 ) {
+    //         slideIndex = slides.length;
+    //     }
+    //     slides.forEach(item => {
+    //         item.style.display = 'none';
+    //     });
+    //     slides[slideIndex - 1].style.display = 'block';
+
+    //     if (slides.length < 10){
+    //         current.textContent = ` 0${slideIndex }`;
+    //     } else {
+    //         current.textContent = slideIndex ;
+    //     }
+    // }
+
+    // prev.addEventListener('click', () => {
+    //     slideShow(slideIndex -= 1);
+
+    // });
+
+    // next.addEventListener('click', ()=> {
+    //     slideShow(slideIndex += 1);
+    // });
 
     // slideShow(slideIndex);
 
